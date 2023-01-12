8c4b965697c1376ea4f5a3ac1682ea903e7f0446
Florian <florian-nicolas.da-silva@orange.fr>
2023-01-12 11:22:38 +0100

    add routes

--- api/app/routes.php
+++ api/app/routes.php
@@ -17,7 +17,53 @@ return function (App $app) {
 
     $app->get('/', function (Request $request, Response $response) {
         // return JSON
-        $data = array('message' => 'Hello world');
+        $data = array('message' => 'Hello worlds lol mathieu');
+        $response = $response->withHeader('Content-Type', 'application/json');
+        $response->getBody()->write(json_encode($data));
+        return $response;
+    });
+
+    $app->get('/create_quiz/{id}', function (Request $request, Response $response, $args) {
+        // return 
+        $quiz_name=$args['id'];
+        system($quiz_bin_dir."create_quiz.sh ".$quiz_name." &",$returnval);
+        $data = array('message' => 'Correction quizz '.+$quiz_name.+$returnval);
+        $response = $response->withHeader('Content-Type', 'application/json');
+        $response->getBody()->write(json_encode($data));
+        return $response;
+    });
+    $app->get('/prepare_correction/{id}', function (Request $request, Response $response, $args) {
+        // return 
+        $quiz_name=$args['id'];
+        system($quiz_bin_dir."prepare_correction.sh ".$quiz_name." &",$returnval);
+        $data = array('message' => 'Correction quizz '.+$quiz_name.+$returnval);
+        $response = $response->withHeader('Content-Type', 'application/json');
+        $response->getBody()->write(json_encode($data));
+        return $response;
+    });
+    $app->get('/correct_quiz/{id}', function (Request $request, Response $response, $args) {
+        // return 
+        $quiz_name=$args['id'];
+        system($quiz_bin_dir."create_quiz.sh ".$quiz_name." &",$returnval);
+        $data = array('message' => 'Correction quizz '.+$quiz_name.+$returnval);
+        $response = $response->withHeader('Content-Type', 'application/json');
+        $response->getBody()->write(json_encode($data));
+        return $response;
+    });
+    $app->get('/omr/{id}', function (Request $request, Response $response, $args) {
+        // return 
+        $quiz_name=$args['id'];
+        system($quiz_bin_dir."omr.sh ".$quiz_name." &",$returnval);
+        $data = array('message' => 'Correction quizz '.+$quiz_name.+$returnval);
+        $response = $response->withHeader('Content-Type', 'application/json');
+        $response->getBody()->write(json_encode($data));
+        return $response;
+    });
+    $app->get('/omr_errors_resolved/{id}', function (Request $request, Response $response, $args) {
+        // return 
+        $quiz_name=$args['id'];
+        system($quiz_bin_dir."omr_errors_resolved.sh ".$quiz_name." &",$returnval);
+        $data = array('message' => 'Correction quizz '.+$quiz_name.+$returnval);
         $response = $response->withHeader('Content-Type', 'application/json');
         $response->getBody()->write(json_encode($data));
         return $response;
